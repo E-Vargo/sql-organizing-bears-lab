d64a6fe721a5ac598ca85640a32797d77ab22916
E-Vargo <vargoelliott@gmail.com>
2021-12-20 11:50:48 -0800

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+contact_id INTEGER PRIMARY KEY,
+id INTEGER,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (5, "Mr. Chocolate", 9, "M", "black", "goofy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (3, "Rowdy", 9, "M", "brown", "happy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (2, "Tabitha", 9, "F", "brown", "goofy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (10, "Sergeant Brown", 9, "M", "brown", "happy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (9, "Melissa", 9, "F", "black", "happy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (8, "Grinch", 9, "M", "brown", "mean", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (8, "Wendy", 9, "F", "brown", "happy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES (6, NULL, 9, "M", "brown", "rude", 1);
